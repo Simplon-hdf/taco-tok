278b1d4fbb548d07f14860a8547815a4a14339be
AlexGitCode <lineale@hotmail.com>
2023-12-06 15:03:26 +0100

    feat(database): new migration prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
-import { ProductModule } from './orders/products.module';
-import { UserModule } from './orders/users.module';
-import { BelongModule } from './belong/belong.module';
+import { ProductModule } from './product/products.module';
+import { UserModule } from './User/users.module';
 @Module({
-  imports: [OrdersModule, ProductModule, UserModule, BelongModule],
+  imports: [OrdersModule, ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
