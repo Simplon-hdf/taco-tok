6ca63c088451b00e79f9a279f3656180c1b5f3da
AlexGitCode <lineale@hotmail.com>
2023-12-06 11:46:07 +0100

    feat: complete order resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
-
+import { ProductModule } from './orders/products.module';
+import { UserModule } from './orders/users.module';
 @Module({
-  imports: [OrdersModule],
+  imports: [OrdersModule, ProductModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
